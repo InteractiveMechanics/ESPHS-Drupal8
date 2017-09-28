45a2e1de3d3eaa9f573c6f99103a0481b07bef49
Michael Tedeschi <mike@interactivemechanics.com>
2017-09-28 13:18:12 -0400

    Add promo banner, fix events page

--- js/main.js
+++ js/main.js
@@ -32,7 +32,41 @@ $(function(){
         $('.site-switcher-section').slideToggle();
     });
 
-    
+
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return null;
+    }
+
+    var cookie = getCookie('hidePopup');
+    if (cookie == null || false){
+        console.log('yes');
+        $('#promo').removeClass('hidden');
+        setTimeout(function(){ $('#promo').addClass('in'); }, 100);
+    }
+    $(document).on('click tap', '.promo-close', function(e) {
+        e.preventDefault();
+        $('#promo').addClass('hidden');
+        setCookie('hidePopup', true, 7);
+    });
+
 
     $('.watch-preview').click(function(){
         var video = $(this).data('video');
